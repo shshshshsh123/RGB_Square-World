19e0cf869879934bffb1722fb48baffde4065fa4
cookie <gihyun700@naver.com>
2025-10-29 14:02:42 +0900

    (#106-2-3) chore: Update
    업데이트

--- Assets/Scripts/Characters/Monster/BaseMonster.cs
+++ Assets/Scripts/Characters/Monster/BaseMonster.cs
@@ -1,46 +1,26 @@
 using UnityEngine;
+using UnityEngine.AI;
 
-public abstract class BaseMonster : MonoBehaviour
+public class BaseMonster : MonoBehaviour
 {
-    [Header("이동")]
-    public float moveSpeed = 5f;
-    public float rotationSpeed = 5f;
-
-    [Header("공격")]
-    public float attackRange = 3f;
-    protected float _attackCoolDown = 2f;
-    protected float _lastAttackTime = 0f;
-
     protected Transform _player;
-    protected Rigidbody _rigidBody;
+    protected NavMeshAgent _agent;
 
     protected void Start()
     {
-        _rigidBody = GetComponent<Rigidbody>();
-        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        _agent = GetComponent<NavMeshAgent>();
+        GameObject playerObject = GameObject.FindWithTag("Player");
         if (playerObject != null)
+        {
             _player = playerObject.transform;
+        }
     }
 
     void FixedUpdate()
     {
-        if (_player == null)
-            return;
-
-        float distanceToPlayer = Vector3.Distance(transform.position, _player.position);
-
-        // 사거리 안에 있으면 공격, 사거리 밖에 있으면 이동
-        if (distanceToPlayer < attackRange)
+        if (_player != null && _agent.isActiveAndEnabled)
         {
-
+            _agent.SetDestination(_player.position);
         }
-            
-        else
-        {
-
-        }
-            
     }
-
-    
 }
