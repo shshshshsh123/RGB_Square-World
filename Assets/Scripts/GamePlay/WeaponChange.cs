3fe3cc2aa8bde763891b568256db697186af70e8
shshshshsh123 <80195292+shshshshsh123@users.noreply.github.com>
2025-11-12 13:03:44 +0900

    (#108-1-5) feat: UM
    실수로 덜보냄

--- Assets/Scripts/GamePlay/WeaponChange.cs
+++ Assets/Scripts/GamePlay/WeaponChange.cs
@@ -1,13 +1,29 @@
 using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
 
 public class WeaponChange : MonoBehaviour
 {
     [SerializeField] GameObject _weaponPanel;
+    [SerializeField] GameObject _keyCanvas;
+    [SerializeField] Vector3 _keyCanvasOffset;
     bool _canAction = false;
+
+    [Header("# UI")]
+    [SerializeField] TMP_Text[] _texts; // 0: Melee, 1: Range, 2: Magic
+
+    private PlayerAttack _playerAttack; // 무기 정보 확인용
 
     private void Update()
     {
         OpenWeaponPanel();
+    }
+
+    private void LateUpdate()
+    {
+        _keyCanvas.transform.position = transform.position + _keyCanvasOffset;
+        _keyCanvas.transform.LookAt(_keyCanvas.transform.position + Camera.main.transform.forward);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -15,6 +31,8 @@ public class WeaponChange : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             _canAction = true;
+            _keyCanvas.SetActive(true);
+            _playerAttack = other.GetComponent<PlayerAttack>();
         }
     }
 
@@ -23,15 +41,54 @@ public class WeaponChange : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             _canAction = false;
+            _keyCanvas.SetActive(false);
         }
     }
 
     void OpenWeaponPanel()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F) && _canAction)
         {
             _weaponPanel.SetActive(true);
+            if (_playerAttack.equippedWeapons[0] != null)
+            {
+                int type = (int)_playerAttack.equippedWeapons[0].weaponData.weaponType;
+                _texts[type].text = "Lv." + _playerAttack.equippedWeapons[0].currentLevel.ToString() + " -> Lv." + (_playerAttack.equippedWeapons[0].currentLevel + 1).ToString();
+            }
             Time.timeScale = 0.0f;  // UI 열리면 시간이 멈춘다!
         }
+    }
+
+    public void ChangeWeapon(int type)  // 0: Melee, 1: Range, 2: Magic
+    {
+        if (_playerAttack.equippedWeapons == null) _playerAttack.equippedWeapons = new List<PlayerAttack.EquippedWeapon>();
+        for (int i = 0; i < 3; i++)
+        {
+            _texts[i].text = "새로 획득!";
+        }
+
+        if (_playerAttack.equippedWeapons.Count > 0)
+        {
+            // 무기가 같은걸 눌럿으면 레벨업 합니다.
+            if (type == (int)_playerAttack.equippedWeapons[0].weaponData.weaponType)
+            {
+                _playerAttack.AddOrUpgradeWeapon(_playerAttack.weaponDatas[type]); // 일단은 임시로 이렇게 처리함;; 좀 더럽긴한데
+            }
+            // 다른 무기 골랏으면 무기 다 지우고 다시 새거 획득합니다다다다.
+            else
+            {
+                _playerAttack.equippedWeapons.Clear();
+                _playerAttack.AddOrUpgradeWeapon(_playerAttack.weaponDatas[type]);
+            }
+        }
+        else
+        {
+            // 이건 무기 처음 획득할때
+            _playerAttack.AddOrUpgradeWeapon(_playerAttack.weaponDatas[type]);
+        }
+
+        // 고르는게 끝나며는?
+        _weaponPanel.SetActive(false);
+        Time.timeScale = 1.0f;
     }
 }
